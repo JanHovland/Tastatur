09fbf25da60a3a088b61d5d0fa83e605d903cbea
Jan Hovland <jan.hovland@lyse.net>
2018-12-14 13:07:47 +0100

    Initial Commit

---
+++ Tastatur/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tastatur
+//
+//  Created by Jan  on 14/12/2018.
+//  Copyright © 2018 Jan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
